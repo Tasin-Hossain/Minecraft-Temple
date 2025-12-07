645426d1b0ec9225f6ba12e41d23f22495645c1a
ShohagBasak <shohagbasak225@gmail.com>
2025-12-07 14:42:54 +0600

    changing Home jsx

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -4,10 +4,13 @@ import { IoMdContacts } from "react-icons/io";
 import { FaEnvelopeOpenText } from "react-icons/fa6";
 import Input from '../Components/ui/Input/Input';
 import minecraftData from '../Api/Minecraft.json';
+import pluginsData from '../Api/Plugins.json';
 import { ToastContainer, toast } from 'react-toastify';
+import faqData from '../Api/Faq.json';
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
+import Button from '../Components/ui/Button/Button';
 
 
 
@@ -27,7 +30,7 @@ const Home = () => {
     return () => clearInterval(interval);
   }, [words.length]);
 
-const notify = () => toast("Downloading your file!");
+  const notify = () => toast("Downloading your file!");
 
   return (
     <div className='bg-gray-900 p-4 space-y-5'>
@@ -87,121 +90,92 @@ const notify = () => toast("Downloading your file!");
         ))}
       </div>
       {/* Plugin sec */}
-      <div className='px-5'>
+      <div className='px-5 pb-5'>
         <div className='mb-3'>
           <h2 className='text-2xl font-bold'>Latest Minecraft Plugins</h2>
           <p className='font-medium'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque obcaecati, consequatur molestiae quo nulla eius.</p>
         </div>
-        {/* plugins card sec */}
-        <div className='grid grid-cols-4 gap-4'>
-          {/* plugins card 1 */}
-          <div className='flex flex-col items-stretch gap-2 bg-gray-700 rounded-2xl'>
-            <div className='overflow-hidden p-2'>
-              <img className='w-full h-full object-cover rounded-2xl' src="/src/Assets/Mc slide 1.jpg" alt="" />
-            </div>
-            <div>
-              <h2 className='text-2xl font-bold text-amber-600 px-3'>Custom Items</h2>
-              <p className='text-amber-100 mb-3 px-3'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque, ab.</p>
-              <div className='flex gap-3 items-center justify-between px-6 mb-4'>
-                <div className='text-center'>
-                  <h3 className='text-2xl font-bold'>200</h3>
-                  <h4 className='font-medium'>Views</h4>
+        {/* plugins card slider sec */}
+        <Swiper
+          spaceBetween={15}
+          slidesPerView={4}
+          className="w-full px-2"
+        >
+          {pluginsData.map((item) => (
+            <SwiperSlide key={item.id} className="w-[250px]">
+              <div className='flex flex-col items-stretch gap-2 bg-gray-700 rounded-2xl'>
+
+                <div className='overflow-hidden p-2'>
+                  <img
+                    className='w-full h-[150px] object-cover rounded-2xl'
+                    src={item.image}
+                    alt={item.title}
+                  />
                 </div>
-                <div className='text-center'>
-                  <h3 className='text-2xl font-bold'>200</h3>
-                  <h4 className='font-medium'>Download</h4>
+
+                <div>
+                  <h2 className='text-2xl font-bold text-amber-600 px-3'>
+                    {item.title}
+                  </h2>
+
+                  <p className='text-amber-100 mb-3 px-3'>
+                    A unique Minecraft plugin to improve your server experience.
+                  </p>
+
+                  <div className='flex gap-3 items-center justify-between px-6 mb-4'>
+                    <div className='text-center'>
+                      <h3 className='text-2xl font-bold'>{item.views}</h3>
+                      <h4 className='font-medium'>Views</h4>
+                    </div>
+
+                    <div className='text-center'>
+                      <h3 className='text-2xl font-bold'>{item.downloads}</h3>
+                      <h4 className='font-medium'>Download</h4>
+                    </div>
+
+                    <div className='text-center'>
+                      <h3 className='text-2xl font-bold'>{item.rating}</h3>
+                      <h4 className='font-medium'>Rating</h4>
+                    </div>
+                  </div>
+
+                  <div className='text-center px-2 mb-3'>
+                    <Button
+                      onClick={notify}
+                      className='bg-amber-600 rounded-2xl p-2 w-full text-white'
+                    >
+                      Download
+                    </Button>
+                  </div>
                 </div>
-                <div className='text-center'>
-                  <h3 className='text-2xl font-bold'>2.2</h3>
-                  <h4 className='font-medium'>Rating</h4>
-                </div>
+
               </div>
-              <div className='text-center px-2 mb-3'>
-                <button onClick={notify} className='bg-amber-600 rounded-2xl p-2 w-full text-white'>Download</button>
-                <ToastContainer />
-              </div>
-            </div>
-          </div>
-          {/* plugins card 2 */}
-          <div className='flex flex-col items-stretch gap-2 bg-gray-700 rounded-2xl'>
-            <div className='overflow-hidden p-2'>
-              <img className='w-full h-full object-cover rounded-2xl' src="/src/Assets/Mc slide 1.jpg" alt="" />
-            </div>
-            <div>
-              <h2 className='text-2xl font-bold text-amber-600 px-3'>Custom Items</h2>
-              <p className='text-amber-100 mb-3 px-3'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque, ab.</p>
-              <div className='flex gap-3 items-center justify-between px-6 mb-4'>
-                <div className='text-center'>
-                  <h3 className='text-2xl font-bold'>200</h3>
-                  <h4 className='font-medium'>Views</h4>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+      {/* faq sec */}
+      <div className='px-5'>
+        <div>
+          <h2 className='text-2xl font-bold'>Why Download with Minecraft Temple?</h2>
+          <p className='font-medium'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos placeat doloremque illo esse! Ea, iste eaque. Officiis eius voluptatem quaerat.</p>
+        </div>
+        <div>
+          <div className='grid grid-cols-4 gap-4'>
+            {
+              faqData.map((faq) => (
+                <div className='border bg-gray-800 p-2 rounded-2xl border-gray-600'>
+                  <div className='inline-flex items-center justify-center mb-2'>
+                    <img src={faq.icon_description} alt="" />
+                    <h3 className='font-semibold'>{faq.title}</h3>
+                  </div>
+                  <div>
+                    <p className='font-medium'>{faq.description}</p>
+                  </div>
                 </div>
-                <div className='text-center'>
-                  <h3 className='text-2xl font-bold'>200</h3>
-                  <h4 className='font-medium'>Download</h4>
-                </div>
-                <div className='text-center'>
-                  <h3 className='text-2xl font-bold'>2.2</h3>
-                  <h4 className='font-medium'>Rating</h4>
-                </div>
-              </div>
-              <div className='text-center px-2 mb-3'>
-                <button onClick={notify} className='bg-amber-600 rounded-2xl p-2 w-full text-white'>Download</button>
-              </div>
-            </div>
-          </div>
-          {/* plugins card 3 */}
-          <div className='flex flex-col items-stretch gap-2  bg-gray-700 rounded-2xl'>
-            <div className='overflow-hidden p-2'>
-              <img className='w-full h-full object-cover rounded-2xl' src="/src/Assets/Mc slide 1.jpg" alt="" />
-            </div>
-            <div>
-              <h2 className='text-2xl font-bold text-amber-600 px-3'>Custom Items</h2>
-              <p className='text-amber-100 mb-3 px-3'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque, ab.</p>
-              <div className='flex gap-3 items-center justify-between px-6 mb-4'>
-                <div className='text-center'>
-                  <h3 className='text-2xl font-bold'>200</h3>
-                  <h4 className='font-medium'>Views</h4>
-                </div>
-                <div className='text-center'>
-                  <h3 className='text-2xl font-bold'>200</h3>
-                  <h4 className='font-medium'>Download</h4>
-                </div>
-                <div className='text-center'>
-                  <h3 className='text-2xl font-bold'>2.2</h3>
-                  <h4 className='font-medium'>Rating</h4>
-                </div>
-              </div>
-              <div className='text-center px-2 mb-3'>
-                <button onClick={notify} className='bg-amber-600 rounded-2xl p-2 w-full text-white'>Download</button>
-              </div>
-            </div>
-          </div>
-          {/* plugins card 4 */}
-          <div className='flex flex-col items-stretch gap-2  bg-gray-700 rounded-2xl'>
-            <div className='overflow-hidden p-2'>
-              <img className='w-full h-full object-cover rounded-2xl' src="/src/Assets/Mc slide 1.jpg" alt="" />
-            </div>
-            <div>
-              <h2 className='text-2xl font-bold text-amber-600 px-3'>Custom Items</h2>
-              <p className='text-amber-100 mb-3 px-3'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque, ab.</p>
-              <div className='flex gap-3 items-center justify-between px-6 mb-4'>
-                <div className='text-center'>
-                  <h3 className='text-2xl font-bold'>200</h3>
-                  <h4 className='font-medium'>Views</h4>
-                </div>
-                <div className='text-center'>
-                  <h3 className='text-2xl font-bold'>200</h3>
-                  <h4 className='font-medium'>Download</h4>
-                </div>
-                <div className='text-center'>
-                  <h3 className='text-2xl font-bold'>2.2</h3>
-                  <h4 className='font-medium'>Rating</h4>
-                </div>
-              </div>
-              <div className='text-center px-2 mb-3'>
-                <button onClick={notify} className='bg-amber-600 rounded-2xl p-2 w-full text-white'>Download</button>
-              </div>
-            </div>
+
+              ))
+            }
           </div>
         </div>
       </div>
